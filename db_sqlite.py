27648fc12659263241a05efd095085d3a80c1168
Jason Grout <jason.grout@drake.edu>
2011-01-11 18:06:31 -0800

    Filled out the sqlite db file

--- db_sqlite.py
+++ db_sqlite.py
@@ -1,16 +1,40 @@
 import db
 
 # TODO: implement
+#conn = sqlite3.connect('/tmp/example')
+#c = conn.cursor()
 
-class DB(db.DB):
+#CREATE TABLE test1(input TEXT, output TEXT DEFAULT NULL);
+
+class DB_sqlite(db.DB):    
     def create_cell(self, input):
-        raise NotImplementedError
-    
+        """
+        Insert the input text into the database.
+        """
+        c=self.c.cursor()
+        c.execute("""insert into cells (input,output) values (:input,null)""", 
+                  {'input':input})
+        
     def get_unevaluated_cells(self):
-        raise NotImplementedError
-    
+        """
+        Get cells which still have yet to be evaluated.
+        """
+        c=self.c.cursor()
+        c.execute("""select ROWID, input from cells where output is null;""")
+        return [dict(_id=u, input=v) for u,v in c.fetchall()]
+        
     def get_evaluated_cells(self):
-        raise NotImplementedError
-    
+        """
+        Get inputs and outputs which have been evaluated
+        """
+        c=self.c.cursor()
+        c.execute("""select ROWID, input, output from cells where output is not null;""")
+        return [dict(_id=u, input=v, output=w) for u,v,w in c.fetchall()]
+
     def set_output(self, id, output):
-        raise NotImplementedError
+        """
+        """
+        c=self.c.cursor()
+        c.execute("""insert into cells (input) values (:input) where ROWID=:id""",
+                  {'input': input, 'id': id})
+
