544de2a28b14df99a4382f3594b8e2212064682a
Jason Grout <jason.grout@drake.edu>
2011-02-03 11:04:12 -0600

    Rework how IO is captured to use contexts

--- device.py
+++ device.py
@@ -1,6 +1,38 @@
-import sys, time, traceback, StringIO
+import sys, time, traceback, StringIO, contextlib
 
-def run(db):
+# based on a stackoverflow answer
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    """
+    Reassign stdout to be a StringIO object.
+
+    To use, do something like:
+    
+    with stdoutIO() as s:
+        print "Hello world"
+
+    print("Output: %s"%s.getvalue())
+
+    Exceptions in the code should be re-raised and the stdout should
+    correctly revert to what it was even if an error is raised.
+    """
+    old_stdout = sys.stdout
+    if stdout is None:
+        stdout = StringIO.StringIO()
+    sys.stdout = stdout
+    try:
+        # code in the context is executed when we yield
+        yield stdout
+    except:
+        # any exceptions in the code should get propogated
+        raise
+    finally:
+        # Make sure that no matter what, the stdout reverts to what it
+        # was before this context
+        sys.stdout = old_stdout
+
+
+def run(db, poll_interval=0.1):
     """Run the compute device, querying the database and doing
     relevant work."""
     print "Starting device loop..."
@@ -14,8 +46,7 @@ def run(db):
             except:
                 S = StringIO.StringIO()
                 traceback.print_exc(file=S)
-                S.seek(0)
-                output = S.read()
+                output=S.getvalue()
             # Store the resulting output
             db.set_output(X['_id'], output)
         time.sleep(.1)
@@ -36,6 +67,18 @@ def displayhook_hack(string):
     # This function is all so the last line (or single lines) will
     # implicitly print as they should, unless they are an assignment.
     # If anybody knows a better way to do this, please tell me!
+    
+    # The essential problem seems to be that exec executes the code as
+    # if the code was in a file.  However, we want the last statement
+    # to print out as if it was interactive.  So we have to generate
+    # the code as if it was an interactive statement (by compiling a
+    # "single" interactive statement) and executing that code object.
+
+    # There is a patch on trac that uses the ast module to print out
+    # each line's output or the last line's output.  Alternatively, we
+    # could fork a python process and feed the code in as standard
+    # input and just capture the stdout.
+
     string = string.splitlines()
     i = len(string)-1
     if i >= 0:
@@ -57,13 +100,9 @@ def execute_code(code):
     """Evalue the given code, returning what is sent to stdout."""
     code = displayhook_hack(code)
     print code
-    s0 = sys.stdout
-    sys.stdout = StringIO.StringIO()
-    exec code in namespace
-    sys.stdout.seek(0)
-    ans = str(sys.stdout.read())
-    sys.stdout = s0
-    return ans
+    with stdoutIO() as s:
+        exec code in namespace
+    return s.getvalue()
 
 if __name__ == "__main__":
     import misc
