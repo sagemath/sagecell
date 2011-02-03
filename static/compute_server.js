00486a43744f7df651980959b3f7ffc513c2d112
Jason Grout <jason.grout@drake.edu>
2011-02-03 11:09:08 -0600

    Add jquery and the start of an abstraction of the ajax queries.

---
+++ static/compute_server.js
@@ -0,0 +1,32 @@
+// From sagenb/sagenb/data/sage/js/async_lib.js
+
+function generic_callback(status, response_text) {
+    /* do nothing */
+}
+
+function async_request(url, callback, postvars) {
+    var settings = {
+        url : url,
+        async : true,
+        cache : false,
+        dataType: "json"
+    };
+
+    if ($.isFunction(callback)) {
+        settings.error = function (XMLHttpRequest, textStatus, errorThrown) {
+            callback("failure", errorThrown);
+        };
+        settings.success = function (data, textStatus, jqXHR) {
+            callback("success", data, jqXHR);
+        };
+    }
+
+    if (postvars) {
+        settings.type = "POST";
+        settings.data = postvars;
+    } else {
+        settings.type = "GET";
+    }
+
+    $.ajax(settings);
+}
