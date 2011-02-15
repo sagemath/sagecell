4bf44d4c5a425c4ba974cd190fe9a920f135819d
Jason Grout <jason.grout@drake.edu>
2011-02-14 22:34:47 -0700

    Rewrite the testing to use multiprocessing Pools and the timing context to spawn tests and record both the response time results and the total time it took to evaluate a computation.

--- test/ping_server.py
+++ test/ping_server.py
@@ -6,6 +6,7 @@ from random import random
 from time import sleep, time
 import sys
 import numpy
+from multiprocessing import Pool
 import contextlib
 
 @contextlib.contextmanager
@@ -41,24 +42,34 @@ def timing(results=None):
 
 def compute(iterations=10):
     response_times=[]
+    eval_times=[]
     while iterations>0:
         computation='%s+%s'%(random(), random())
         eval_url='http://127.0.0.1:8080/eval?%s'%urlencode(dict(commands=computation))
-        start_time=time()
-        computation_id=json.load(urlopen(eval_url))[u'computation_id']
-        response_times.append(time()-start_time)
-        poll_url='http://127.0.0.1:8080/output_poll?computation_id=%s'%computation_id
-        output=None
-        while output is None:
-            start_time=time()
-            output=json.load(urlopen(poll_url)).get(u'output', None)
-            response_times.append(time()-start_time)
-            print '.',
-            sleep(0.5)
-        print computation, ':', output
+        with timing(eval_times):
+            with timing(response_times):
+                computation_id=json.load(urlopen(eval_url))[u'computation_id']
+            poll_url='http://127.0.0.1:8080/output_poll?computation_id=%s'%computation_id
+            output=None
+            while output is None:
+                with timing(response_times):
+                    output=json.load(urlopen(poll_url)).get(u'output', None)
+                sleep(0.1)
+        #print computation, ':', output
         iterations-=1
-    return response_times
+    return {'response':response_times, 'eval': eval_times}
 
-if __name__ == "__main__":
-    response_times=compute(int(sys.argv[1]))
-    print 'Average: ', numpy.average(response_times), ', Std Dev: ', numpy.std(response_times)
+if __name__ == '__main__':
+    processes=int(sys.argv[1])
+    iterations=int(sys.argv[2])
+    pool = Pool(processes=processes)
+    result = [pool.apply_async(compute, [iterations]) for _ in range(processes)]
+    response_times=[]
+    eval_times=[]
+    for p in range(processes):
+        results=result[p].get()
+        eval_times.extend(results['eval'])
+        response_times.extend(results['response'])
+
+    print 'RESPONSE: Average: ', numpy.average(response_times), ', Std Dev: ', numpy.std(response_times)
+    print 'EVAL: Average: ', numpy.average(eval_times), ', Std Dev: ', numpy.std(eval_times)
