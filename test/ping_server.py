7d8b8e9457ad025bbf6d0822cddf62c85ce5c437
Jason Grout <jason.grout@drake.edu>
2011-02-14 20:12:37 -0700

    Initial implementation of a script to ping the server, make a computation, and retrieve a result.  The script prints out the average and standard deviation of response times.

---
+++ test/ping_server.py
@@ -0,0 +1,29 @@
+
+from urllib2 import urlopen
+from urllib import urlencode
+import json
+from random import random
+from time import sleep, time
+import sys
+import numpy
+
+if __name__ == "__main__":
+    iterations=int(sys.argv[1])
+    response_times=[]
+    while iterations>0:
+        computation='%s+%s'%(random(), random())
+        eval_url='http://127.0.0.1:8080/eval?%s'%urlencode(dict(commands=computation))
+        start_time=time()
+        computation_id=json.load(urlopen(eval_url))[u'computation_id']
+        response_times.append(time()-start_time)
+        poll_url='http://127.0.0.1:8080/output_poll?computation_id=%s'%computation_id
+        output=None
+        while output is None:
+            start_time=time()
+            output=json.load(urlopen(poll_url)).get(u'output', None)
+            response_times.append(time()-start_time)
+            print '.',
+            sleep(0.5)
+        print computation, ':', output
+        iterations-=1
+    print 'Average: ', numpy.average(response_times), ', Std Dev: ', numpy.std(response_times)
