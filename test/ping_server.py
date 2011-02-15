59a8f1508018c5fb0e79aa8088bd216a66f5fe8b
Jason Grout <jason.grout@drake.edu>
2011-02-14 22:29:23 -0700

    Make a timing context manager that records the elapsed time.

--- test/ping_server.py
+++ test/ping_server.py
@@ -6,6 +6,38 @@ from random import random
 from time import sleep, time
 import sys
 import numpy
+import contextlib
+
+@contextlib.contextmanager
+def timing(results=None):
+    """
+    Time the execution of the block of code. If a results list is
+    passed in, the time is appended to the list. Also returns a list
+    of one element containing the time the execution took.
+
+    To use, do something like:
+
+    from time import sleep
+    results_list=[]
+    with timing(results_list) as t:
+        sleep(1)
+    print results_list, t
+
+    Exceptions in the code should be re-raised and the timing should
+    correctly be set regardless of the exceptions.
+    """
+    from time import time
+    try:
+        # code in the context is executed when we yield
+        start=[time()]
+        yield start
+    except:
+        # any exceptions in the code should get propogated
+        raise
+    finally:
+        start[0]=time()-start[0]
+        if results is not None:
+            results.append(start[0])
 
 def compute(iterations=10):
     response_times=[]
