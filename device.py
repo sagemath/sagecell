a35fa2a626634d915ee4e75a397b1844dadb93a5
Jason Grout <jason.grout@drake.edu>
2011-03-01 21:05:34 -0600

    Get rid of an extra space introduced by using the Python 2 print statement.

--- device.py
+++ device.py
@@ -74,9 +74,10 @@ STREAM_SEPARATOR='____NEW__STREAM____'
 HEADER_SEPARATOR='____END_STREAM_HEADER___'
 
 def new_stream(stream_type):
-    print STREAM_SEPARATOR,
-    print r"""{"type":"%s"}"""%stream_type
-    print HEADER_SEPARATOR,
+    import sys
+    sys.stdout.write(STREAM_SEPARATOR)
+    sys.stdout.write(r"""{"type":"%s"}"""%stream_type)
+    sys.stdout.write(HEADER_SEPARATOR)
 
 import json
 
