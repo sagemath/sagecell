43d6426d67c573b348152fad9848391e3a2e4d09
Jason Grout <jason.grout@drake.edu>
2011-03-19 20:42:41 -0500

    Massively revamp the ping_server script to be a good test script for the multi-mechanize.py package.

--- test/ping_server.py
+++ test/ping_server.py
@@ -8,6 +8,9 @@ import sys
 import numpy
 from multiprocessing import Pool
 import contextlib
+import traceback
+
+maxint=2**30
 
 @contextlib.contextmanager
 def timing(results=None):
@@ -36,40 +39,58 @@ def timing(results=None):
         # any exceptions in the code should get propogated
         raise
     finally:
-        start[0]=time()-start[0]
+        start.append(time()-start[0])
         if results is not None:
-            results.append(start[0])
+            results.append(start)
 
-def compute(iterations=10):
-    response_times=[]
-    eval_times=[]
-    while iterations>0:
-        computation='%s+%s'%(random(), random())
+class Transaction(object):
+    def __init__(self):
+        self.custom_timers={}
+
+
+    def run(self):
+        computation_times=[]
+        response_times=[]
+        a=int(random()*maxint)
+        b=int(random()*maxint)
+        computation='print %d+%d'%(a,b)
         eval_url='http://127.0.0.1:8080/eval?%s'%urlencode(dict(commands=computation))
-        with timing(eval_times):
+        with timing(computation_times):
             with timing(response_times):
                 computation_id=json.load(urlopen(eval_url))[u'computation_id']
             poll_url='http://127.0.0.1:8080/output_poll?computation_id=%s'%computation_id
             output=None
-            while output is None:
-                with timing(response_times):
-                    output=json.load(urlopen(poll_url)).get(u'output', None)
+            while True:
                 sleep(0.1)
-        #print computation, ':', output
-        iterations-=1
-    return {'response':response_times, 'eval': eval_times}
+                with timing(response_times):
+                    response=urlopen(poll_url).read()
+                r=json.loads(response)
+                if ('output' in r 
+                    and 'closed' in r['output'] 
+                    and r['output']['closed'] is True):
+                    ans=int(r['output']['stream_0']['content'])
+                    if a+b!=ans:
+                        print "COMPUTATION NOT CORRECT"
+                        raise ValueError("Computation not correct: %s+%s!=%s, off by %s "%(a,b,ans, ans-a-b))
+                    else:
+                        break
+        self.custom_timers['Computation']=computation_times
+        self.custom_timers['Response']=response_times
 
 if __name__ == '__main__':
-    processes=int(sys.argv[1])
-    iterations=int(sys.argv[2])
-    pool = Pool(processes=processes)
-    result = [pool.apply_async(compute, [iterations]) for _ in range(processes)]
-    response_times=[]
-    eval_times=[]
-    for p in range(processes):
-        results=result[p].get()
-        eval_times.extend(results['eval'])
-        response_times.extend(results['response'])
+    trans = Transaction()
+    trans.run()
+    print trans.custom_timers
+    # processes=int(sys.argv[1])
+    # iterations=int(sys.argv[2])
+    # pool = Pool(processes=processes)
+    # result = [pool.apply_async(compute, [iterations]) for _ in range(processes)]
+    # response_times=[]
+    # eval_times=[]
+    # for p in range(processes):
+    #     results=result[p].get()
+    #     eval_times.extend(results['eval'])
+    #     response_times.extend(results['response'])
 
-    print 'RESPONSE: Average: ', numpy.average(response_times), ', Std Dev: ', numpy.std(response_times)
-    print 'EVAL: Average: ', numpy.average(eval_times), ', Std Dev: ', numpy.std(eval_times)
+    # print 'RESPONSE: Average: ', numpy.average(response_times), ', Std Dev: ', numpy.std(response_times)
+    # print 'EVAL: Average: ', numpy.average(eval_times), ', Std Dev: ', numpy.std(eval_times)
