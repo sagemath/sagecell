3f6d80ac81f25fef02bffea40b7f59545ddb1857
Jason Grout <jason.grout@drake.edu>
2011-04-12 22:26:19 -0500

    Make javascript detect tabs in Chrome and other browsers
    See http://api.jquery.com/keydown/ and http://api.jquery.com/keypress/, which indicate that keydown is the non-repeating cross-browser event to trigger, and http://api.jquery.com/event.which/, which indicates we should use the normalized event.which for the keycode.

--- static/completion.js
+++ static/completion.js
@@ -1,17 +1,18 @@
 function handleKeyPress(event) {
-    if(event.keyCode==9) {
+    if(event.which==9) {
         event.preventDefault();
         var box=event.target;
         var i=box.value.lastIndexOf("\n",box.selectionStart-1);
-        if(!box.value.substring(i,box.selectionStart).match(/^\s*$/))
+        if(!box.value.substring(i,box.selectionStart).match(/^\s*$/)) {
             $.getJSON("/complete",{code:box.value, pos:box.selectionStart},showCompletions);
+	}
         else {
             // Insert a tab at the current position and reset the cursor position
             var start=box.selectionStart;
             box.value=box.value.substring(0,start)+"\t"+box.value.substring(start);
             box.selectionStart=box.selectionEnd=start+1;
         }
-    } else if(event.keyCode==13 && event.shiftKey) {
+    } else if(event.which==13 && event.shiftKey) {
         event.preventDefault();
         $("#evalButton").submit();
     }
@@ -23,7 +24,7 @@ function showCompletions(data,textStatus,jqXHR) {
 
 function setUpHandler() {
     if($("#commands").length)
-        $("#commands").keypress(handleKeyPress);
+        $("#commands").keydown(handleKeyPress);
 }
 
 $(document).ready(setUpHandler);
