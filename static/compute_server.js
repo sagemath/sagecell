2160417ab503ec3fe1a5b6211fade3ca4befe7c1
Jason Grout <jason.grout@drake.edu>
2011-05-13 08:56:29 -0500

    Clean up javascript file (delete comments/whitespace, convert if/else to switch)

--- static/compute_server.js
+++ static/compute_server.js
@@ -72,66 +72,36 @@ function get_output_success(data, textStatus, jqXHR, id) {
                 sequence=0;
             }
             // Handle each stream type.  This should probably be separated out into different functions.
-            if(msg.msg_type==='stream'){
+	    switch(msg.msg_type) {
+	    case 'stream': 
+                $('#output').append("<pre class='"+msg.content.name+"'>"+msg.content.data+"</pre>");
+		break;
 
-                switch(msg.content.name) {
-                case 'stdout':
-                    $('#output').append("<pre class='stdout'>"+msg.content.data+"</pre>");
-                    break;
+	    case 'pyout':
+                $('#output').append("<pre class='pyout'>"+msg.content.data['text/plain']+"</pre>");
+		break;
 
-                case 'stderr':
-                    $('#output').append("<pre class='stderr'>"+msg.content.data+"</pre>");
-                    break;
-
-               
-                }
-            } else if(msg.msg_type==='pyout') {
-                $('#output').append("<pre class='pyout'>"+msg.content.data['text/plain']+"</pre>")
-            } else if(msg.msg_type==='display_data') {
+	    case 'display_data':
                 if(msg.content.data['image/svg+xml']!==undefined) {
                     $('#output').append('<object id="svgImage" type="image/svg+xml">'+msg.content.data['image/svg+xml']+'</object>');
                 } else if(msg.content.data['text/html']!==undefined) {
 		    $('#output').append('<div>'+msg.content.data['text/html']+'</div>');
 		}
-            } else if(msg.msg_type==='files') {
+		break;
+
+	    case 'files':
 		var html="<div>\n";
 		for(var j in msg.content.files)
-		    html+="<a href=\"/files/"+id+"/"+msg.content.files[j]+"\">"+
-		          msg.content.files[j]+"</a><br>\n";
+		    html+="<a href=\"/files/"+id+"/"+msg.content.files[j]+"\">"+msg.content.files[j]+"</a><br>\n";
 		$('#output').append(html);
+		break;
 	    }
 	    
 	    // Append the message to the div of messages
 	    $('#messages').append(document.createElement('div'))
 		.children().last().text(JSON.stringify(msg));
-            //TODO: handle all the types of messages intelligently
         }
     }
-/*
-    for(d in data.output) {
-            if(d!="closed")
-                streams.push(data.output[d]);
-    }
-    streams.sort(sortstream);
-    //$('#output').empty();
-    for(i in streams)
-        if(streams[i].type=='text')
-            $('#output').append("<pre>"+streams[i].content+"</pre>");
-        else if(streams[i].type=='image')
-            for(j in streams[i].files)
-                $('#output').append("<img src='/files/"+id+"/"+streams[i].files[j]+"'/><br/>");
-        else if(streams[i].type=='files') {
-            var toAppend="<div>";
-            for(j in streams[i].files)
-                toAppend+="<a href='/files/"+id+"/"+streams[i].files[j]+"'>"+
-                    streams[i].files[j]+"</a><br/>";
-            $('#output').append(toAppend+"</div>");
-        }
-        else if(streams[i].type=='error')
-            $('#output').append("<pre class='error'>"+streams[i].ename+"<br/>"+streams[i].evalue+"</pre>")
-          if(data.output && !data.output.closed)
-              $('#output').append("&hellip;")
-*/
     if(!done) {
         // poll again after a bit
         setTimeout(function() {get_output(id);}, 2000);
