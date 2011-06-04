72ebc57d8e535c9fca870f51afece885fb47fc2a
Jason Grout <jason.grout@drake.edu>
2011-06-04 10:20:39 -0500

    Add a write_process_id function, but usually I think it is sufficient to look in the database.

--- util.py
+++ util.py
@@ -4,3 +4,10 @@ def log(message, key=' '):
     if LOGGING:
         sys.__stdout__.write("%s\t: %s\n"%(key, message))
 
+import os
+DEFAULT_DIR=''
+
+def write_process_id(prefix=None):
+    with open(DEFAULT_DIR+prefix+'single-cell-pid.%d'%os.getpid(),'w') as f:
+        f.write('%d %d %d\n'%(os.getpid(), os.getpgid(0), os.getppid()))
+
