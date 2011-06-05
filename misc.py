8f47a47059bc526b072a744ccb9cddf6ffab5a68
Jason Grout <jason.grout@drake.edu>
2011-06-04 23:45:13 -0500

    Support a config file for the mongodb database info.

--- misc.py
+++ misc.py
@@ -1,5 +1,13 @@
 import sys
 
+try:
+    import singlecell_conf
+    from singlecell_conf import config
+except ImportError:
+    config=False
+    pass
+
+
 def select_db(sysargs, context=None):
     db=sysargs.db
     if db=="sqlite":
@@ -15,8 +23,18 @@ def select_db(sysargs, context=None):
         return db_sqlalchemy.DB('sqlalchemy_sqlite.db'), None # None should be replaced by the sqlite filestore
     elif db=="mongo":
         import pymongo, db_mongo, filestore
-        connection=pymongo.Connection().demo
-        return db_mongo.DB(connection), filestore.FileStoreMongo(connection)
+        if config:
+            if '@' in config['mongo_uri']:
+                # password specified, so we need to include the database in the URI
+                URI=config['mongo_uri']+'/'+config['mongo_db']
+            else:
+                URI = config['mongo_uri']
+            print URI
+            conn=pymongo.Connection(URI)
+            database=pymongo.database.Database(conn, config['mongo_db'])
+        else:
+            database=pymongo.Connection().demo
+        return db_mongo.DB(database), filestore.FileStoreMongo(database)
     elif db=="zmq":
         import db_zmq, filestore
         return db_zmq.DB(address=sysargs.dbaddress), filestore.FileStoreZMQ(address=sysargs.fsaddress)
