67f859521864b25fda28e50aa891a14c3d66178a
Alex Kramer <kramer.alex.kramer@gmail.com>
2011-06-07 20:36:34 -0400

    Interact documentation
    Documents interact backend, autosyntax guessing, and interact usage/syntax in general.
    Also reorganizes interact_protocol.rst and updates TODO lists.

--- interact.py
+++ interact.py
@@ -3,6 +3,12 @@ _INTERACTS={}
 __single_cell_timeout__=0
 
 def interact(f):
+    """
+    Interprets interact functions and their controls and sends interact
+    start, interact evaluation, and interact end messages.
+
+    :arg f: A function definition.
+    """
     import inspect
     from uuid import uuid4
     from sys import _sage_messages as MESSAGE
@@ -29,39 +35,69 @@ def interact(f):
     MESSAGE.message('interact_end',{})
 
 class InteractControl:
+    """
+    Base class for all interact controls.
+    """
     def __init__(self, *args, **kwargs):
         self.args=args
         self.kwargs=kwargs
 
 class InputBox(InteractControl):
+    """
+    Defines an input box control.
+    """
     def message(self):
+        """
+        :returns: Input box control configuration for an interact_start message.
+        :rtype: Dict
+        """
         return {'control_type':'input_box',
                 'default':self.kwargs.get('default',""),
                 'raw':self.kwargs.get('raw',False),
                 'label':self.kwargs.get('label',"")}
     def default(self):
+        """
+        :returns: Default value of control.
+        """
         return self.kwargs.get('default',None)
 
 class Selector(InteractControl):
+    """
+    Defines a selector interact control.
+    """
     def __init__(self, *args, **kwargs):
         self.kwargs = kwargs
         self.values = self.kwargs.get('values',[0])
         self.default_value = self.kwargs.get('default',0)
     def message(self):
+        """
+        :returns: Selector control configuration for an interact_start message.
+        :rtype: Dict
+        """
         return {'control_type': 'selector',
                 'values': self.values,
                 'default': self.default_value,
                 'raw': self.kwargs.get('raw',False),
                 'label':self.kwargs.get('label',"")}
     def default(self):
+        """
+        :returns: Default value of control.
+        """
         return self.values[self.default_value]
 
 class Slider(InteractControl):
+    """
+    Defines a slider interact control.
+    """
     def __init__(self, *args, **kwargs):
         self.kwargs = kwargs
         self.interval = self.kwargs.get('range',[0,100])
         self.default_value = self.kwargs.get('default',self.interval[0])
     def message(self):
+        """
+        :returns: Slider control configuration for am interact_start message.
+        :rtype: Dict
+        """
         return {'control_type':'slider',
                 'default':self.default_value,
                 'range':self.interval,
@@ -69,16 +105,31 @@ class Slider(InteractControl):
                 'raw':self.kwargs.get('raw',True),
                 'label':self.kwargs.get('label',"")}
     def default(self):
+        """
+        :returns: Default value of control.
+        """
         return self.default_value
 
 def automatic_control(default):
+    """
+    Guesses the desired interact control from the syntax of the parameter.
+    
+    :arg default: Parameter value.
+    
+    :returns: An InteractControl object.
+    :rtype: InteractControl
+    
+    
+    """
     from numbers import Number
     label = ""
     default_value = 0
     
+    # Checks for interact controls that are verbosely defined
     if isinstance(default, InteractControl):
         return default
     
+    # Checks for labels and default values
     for _ in range(2):
         if isinstance(default, tuple) and len(default) == 2 and isinstance(default[0], str):
             label, default = default
@@ -106,7 +157,7 @@ def automatic_control(default):
     return C
 
 
-# aliases for backwards compatibility
+# Aliases for backwards compatibility
 slider=Slider
 selector=Selector
 input_box=InputBox
