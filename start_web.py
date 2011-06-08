1ba04fd98f1dce0889ee650ccc714bc96f22b53b
Jason Grout <jason.grout@drake.edu>
2011-06-08 12:33:36 -0500

    Change the start_web to not include the module argument, as it always stays the same.

--- start_web.py
+++ start_web.py
@@ -2,7 +2,7 @@
 
 import singlecell_config
 
-command='./uwsgi '
+command='./uwsgi --module web_server:app'
 for k,v in singlecell_config.web_server_config.items():
     command+=' --%s %r '%(k,v)
 
