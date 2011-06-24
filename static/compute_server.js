c5a04c0a0062ea3603ba85ff53b1ff5f42cce2ca
Jason Grout <jason.grout@drake.edu>
2011-06-24 08:06:10 -0500

    Make display_data text/filename mimetype get the correct file.

--- static/compute_server.js
+++ static/compute_server.js
@@ -312,6 +312,8 @@ Session.prototype.get_output_success = function(data, textStatus, jqXHR) {
                     this.output('<object id="svgImage" type="image/svg+xml">'+msg.content.data['image/svg+xml']+'</object>',output_block);
                 } else if(msg.content.data['text/html']!==undefined) {
 		    this.output('<div>'+msg.content.data['text/html']+'</div>',output_block);
+		} else if(msg.content.data['text/filename']!==undefined) {
+		    this.output('<img src="'+$URL['root']+'files/'+id+'/'+msg.content.data['text/filename']+'" />');
 		}
 		break;
 
