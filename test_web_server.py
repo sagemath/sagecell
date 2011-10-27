7ad0e13043dde94945a54abee44ca767ea389423
Jason Grout <jason.grout@drake.edu>
2011-10-27 16:39:49 -0500

    Make the tests simpler since we are using nose instead of just the built-in unites.

--- test_web_server.py
+++ test_web_server.py
@@ -1,21 +1,24 @@
-import os
+"""
+Nose tests for the web server
+"""
+
 import web_server
-import unittest
-import tempfile
 
-class SinglecellTestCase(unittest.TestCase):
-
+class TestWebReplies:
     def setUp(self):
         web_server.app.config['TESTING'] = True
         self.app = web_server.app.test_client()
+        # set up temp mongodb instance
 
     def tearDown(self):
+        # clean up temp mongodb instance
         pass
 
     def test_front_page(self):
         rv = self.app.get('/')
         assert '<title>Simple Compute Server</title>' in rv.data
+    
+    def test_config(self):
+        rv = self.app.get('/config')
+        assert 'webserver=' in rv.data
 
-
-if __name__ == '__main__':
-    unittest.main()
