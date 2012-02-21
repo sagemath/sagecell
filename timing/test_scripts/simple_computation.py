88f4d1d95e205fd72100e6fa4a1f27234eb1eb99
Jason Grout <jason.grout@drake.edu>
2012-02-21 16:59:31 -0600

    Add a timeout for the polling, to see if the device is down

--- timing/test_scripts/simple_computation.py
+++ timing/test_scripts/simple_computation.py
@@ -10,6 +10,7 @@ import contextlib
 import traceback
 
 from timing_util import timing, json, json_request
+from time import time
 
 from sagecell import Session
 
@@ -18,8 +19,9 @@ class Transaction(object):
         self.custom_timers={}
         self.MAXRAND=kwargs.get('maxrand', 2**30)
         self.BASE_URL=kwargs.get('base_url', 'http://localhost:8080/')
-        self.POLL_INTERVAL=kwargs.get('poll_interval', 0.1)
-        
+        self.POLL_INTERVAL=kwargs.get('poll_interval', 0.25)
+        self.TIMEOUT=kwargs.get('timeout', 30)
+
     def run(self):
         """
         Ask for the sum of two random numbers and check the result
@@ -32,12 +34,15 @@ class Transaction(object):
         s=Session(self.BASE_URL)
         request=s.prepare_execution_request(code)
         sequence=0
+
         with timing(computation_times):
             with timing(response_times):
                 s.send_execution_request(request)
-
+            start_time = time()
             done=False
             while not done:
+                if time()-start_time>self.TIMEOUT:
+                    raise Exception("TIMEOUT")
                 sleep(self.POLL_INTERVAL)
                 with timing(response_times):
                     r=s.output_poll(sequence)
@@ -67,10 +72,10 @@ if __name__ == '__main__':
     parser.add_argument('--base_url', default='http://localhost:8080',
                         help='the base url for the sage server')
     parser.add_argument('-q','--quiet', dest='quiet', action='store_true')
-
+    parser.add_argument('--timeout', dest='timeout', default=30, type=float)
     args = parser.parse_args()
 
-    trans = Transaction(base_url=args.base_url)
+    trans = Transaction(base_url=args.base_url, timeout=args.timeout)
     trans.run()
     if not args.quiet:
         print trans.custom_timers
