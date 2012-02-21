22dd89632fb957bbc2a73d55b8608015bc8798f9
Jason Grout <jason.grout@drake.edu>
2012-02-21 13:43:22 -0600

    Make the webserver requirements more flexible: add twisted and python options.

--- start_web.py
+++ start_web.py
@@ -2,10 +2,24 @@
 
 import sagecell_config
 
-command='./uwsgi --module web_server:app'
-for k,v in sagecell_config.web_server_config.items():
-    command+=' --%s %r '%(k,v)
+if hasattr(sagecell_config, 'webserver'):
+    webserver = sagecell_config.webserver
+else:
+    webserver = 'python'
+
+if webserver=='uwsgi':
+    command=sagecell_config.uwsgi+' --module web_server:app'
+    for k,v in sagecell_config.uwsgi_config.items():
+        command+=' --%s %r '%(k,v)
+elif webserver=='twistd':
+    command = sagecell_config.twistd+" -n web --wsgi web_server.app"
+    for k,v in sagecell_config.twistd_config.items():
+        command+=' --%s %r '%(k,v)
+elif webserver=='python':
+    # Run in single-user mode
+    command = sagecell_config.python+" ./web_server.py"
 
 import os
 print 'Executing: ', command
 os.system(command)
+
