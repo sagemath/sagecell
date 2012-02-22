f0bf453ac20ac0754349fafc2116c3b02123ac6f
Jason Grout <jason.grout@drake.edu>
2012-02-21 20:32:32 -0600

    create pid files in the start_web and start_device scripts

--- start_device.py
+++ start_device.py
@@ -5,14 +5,17 @@ import uuid
 options=sagecell_config.device_config
 python=options.pop('python')
 
+pidfile=''
 command=python+' trusted_db.py '
 for k,v in options.items():
     if k=='pidfile':
         v+=str(uuid.uuid4())+'.pid'
-        print v
+        pidfile=v
     command+=' --%s %r '%(k,v)
 
 
 import os
 print 'Executing: ', command
 os.system(command)
+if pidfile and os.path.isfile(pidfile):
+    os.remove(pidfile)
--- start_web.py
+++ start_web.py
@@ -7,13 +7,18 @@ if hasattr(sagecell_config, 'webserver'):
 else:
     webserver = 'python'
 
+pidfile=''
 if webserver=='uwsgi':
     command=sagecell_config.uwsgi+' --module web_server:app'
     for k,v in sagecell_config.uwsgi_config.items():
+        if k=='pidfile':
+            pidfile=v
         command+=' --%s %r '%(k,v)
 elif webserver=='twistd':
     command = sagecell_config.twistd+" -n web --wsgi web_server.app"
     for k,v in sagecell_config.twistd_config.items():
+        if k=='pidfile':
+            pidfile=v
         command+=' --%s %r '%(k,v)
 elif webserver=='python':
     # Run in single-user mode
@@ -22,4 +27,5 @@ elif webserver=='python':
 import os
 print 'Executing: ', command
 os.system(command)
-
+if pidfile and os.path.isfile(pidfile):
+    os.remove(pidfile)
