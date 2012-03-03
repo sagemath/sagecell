dbb904ef7a62d48294628bf0b66311001168a781
Ira Hanson <hanson.ira@gmail.com>
2012-03-03 01:42:22 -0600

    Automatically evaluate new cells based on an option given in the arguments of sagecell.makeSageCell, rather than the document’s location

--- templates/embedded_sagecell.js
+++ templates/embedded_sagecell.js
@@ -276,8 +276,7 @@ sagecell.initCell = (function(sagecellInfo) {
     };
 
     inputLocation.find(".sagecell_evalButton").click(sagecellInfo.submit);
-    // Automatically submit code from URL parameter unless parameter "autoeval=false" is given
-    if (sagecellInfo.code && !document.location.search.match(/(^\?|&)autoeval=false(&|$)/)) {
+    if (sagecellInfo.code && sagecellInfo.autoeval) {
 	sagecellInfo.submit();
     }
     return sagecellInfo;
