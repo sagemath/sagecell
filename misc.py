2a50334ecbc2d44988d49995260584f5e3c8c1d9
Jason Grout <jason.grout@drake.edu>
2012-03-20 08:54:14 -0400

    Allow the filestore and database to be different.

--- misc.py
+++ misc.py
@@ -1,5 +1,19 @@
 import sys
 
+def mongo_connection():
+    """
+    Set up a MongoDB connection.
+    """
+    import pymongo
+    mongo_config = sagecell_config.mongo_config
+
+    if '@' in mongo_config['mongo_uri']:
+        # password specified, so we need to include the database in the URI
+        URI=mongo_config['mongo_uri']+'/'+mongo_config['mongo_db']
+    else:
+        URI = mongo_config['mongo_uri']
+    return pymongo.Connection(URI)
+
 def select_db(sysargs=None, context=None):
     u"""
     Create connections to the database and filestore given in sysargs.
@@ -21,20 +35,26 @@ def select_db(sysargs=None, context=None):
             db = fs = 'sqlalchemy'
 
     if db=="sqlalchemy":
-        import db_sqlalchemy, filestore
-        return (db_sqlalchemy.DB(sagecell_config.sqlalchemy_uri),
-                filestore.FileStoreSQLAlchemy(sagecell_config.sqlalchemy_uri))
+        import db_sqlalchemy
+        return_db = db_sqlalchemy.DB(sagecell_config.sqlalchemy_uri)
     elif db=="mongo":
-        import pymongo, db_mongo, filestore
-        mongo_config = sagecell_config.mongo_config
-
-        if '@' in mongo_config['mongo_uri']:
-            # password specified, so we need to include the database in the URI
-            URI=mongo_config['mongo_uri']+'/'+mongo_config['mongo_db']
-        else:
-            URI = mongo_config['mongo_uri']
-        conn=pymongo.Connection(URI)
-        return db_mongo.DB(conn), filestore.FileStoreMongo(conn)
+        import db_mongo
+        conn = mongo_connection()
+        return_db = db_mongo.DB(conn)
     elif db=="zmq":
-        import db_zmq, filestore
-        return db_zmq.DB(address=sysargs.dbaddress), filestore.FileStoreZMQ(address=sysargs.fsaddress)
+        import db_zmq
+        return_db = db_zmq.DB(address=sysargs.dbaddress)
+
+    import filestore
+    if fs=="sqlalchemy":
+        return_fs = filestore.FileStoreSQLAlchemy(sagecell_config.sqlalchemy_uri))
+    elif fs=="mongo":
+        mongo_config = sagecell_config.mongo_config
+        if db!="mongo":
+            # conn has not been set up yet
+            conn = mongo_connection()
+        return_fs = filestore.FileStoreMongo(conn)
+    elif fs=="zmq":
+        return_fs = filestore.FileStoreZMQ(address=sysargs.fsaddress)
+
+    return return_db, return_fs
