5962b1193641399f312982211e04513c3c205141
Ira Hanson <hanson.ira@gmail.com>
2012-03-22 00:52:34 -0500

    Bugfix

--- static/root.js
+++ static/root.js
@@ -2,7 +2,7 @@
     var oldScroll = window.pageYOffset;
     window.addEventListener("scroll", function () {
         if (window.pageYOffset > oldScroll) {
-            document.body.style.height = document.body.clientHeight + "px";
+            document.body.style.height = document.getElementById("sagecell_pageContent").clientHeight + "px";
         } else {
             if (window.pageYOffset > 0) {
                 document.body.style.height = Math.max(window.pageYOffset +  window.innerHeight,
