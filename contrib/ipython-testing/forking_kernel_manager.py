57cadb4bd1ed48af24b83c6c03af041158b0ff04
Ira Hanson <hanson.ira@gmail.com>
2012-06-08 03:54:40 -0500

    Implementation of forking kernel manager

--- contrib/ipython-testing/forking_kernel_manager.py
+++ contrib/ipython-testing/forking_kernel_manager.py
@@ -1,29 +1,53 @@
 import uuid
 import zmq
-from IPython.zmq.kernelmanager import KernelManager
+import os
+import signal
+from IPython.zmq.kernelapp import KernelApp
+from IPython.config.loader import Config
 from multiprocessing import Process, Pipe
 
 class ForkingKernelManager:
     def __init__(self):
-        self.kernel_pipes = {}
+        self.kernels = {}
 
-    def start_kernel(self):
-        p, q = Pipe()
-        kernel_id = str(uuid.uuid4())
-        Process(target=self.fork_kernel, args=(q,)).start()
-        self.kernel_pipes[kernel_id] = p
-        shell, iopub, stdin, hb = p.recv()
-        return {"kernel_id": kernel_id,
-                 "ports": {"shell_port": shell, "iopub_port": iopub,
-                           "stdin_port": stdin, "hb_port": hb}}
+    def start_kernel(self, kernel_id=str(uuid.uuid4()), config=Config()):
+        ka = KernelApp.instance(config=config)
+        ka.initialize()
+        ports = {"shell_port": ka.shell_port, "iopub_port": ka.iopub_port,
+                 "stdin_port": ka.stdin_port, "hb_port": ka.hb_port}
+        print ports
+        proc = Process(target=ka.start)
+        proc.start()
+        self.kernels[kernel_id] = (ka, proc, ports)
+        return {"kernel_id": kernel_id, "ports": ports}
+
+    def send_signal(self, kernel_id, signal):
+        """Send a signal to a running kernel."""
+        if kernel_id in self.kernels:
+            try:
+                os.kill(self.kernels[kernel_id][1].pid, signal)
+                self.kernels[kernel_id][1].join()
+            except OSError, e:
+                # On Unix, we may get an ESRCH error if the process has already
+                # terminated. Ignore it.
+                from errno import ESRCH
+                if e.errno != ESRCH:
+                    raise
 
     def kill_kernel(self, kernel_id):
-        self.kernel_pipes[kernel_id].send("")
-        del self.kernel_pipes[kernel_id]
+        """Kill a running kernel."""
+        self.kernels[kernel_id][0].shell_socket.close()
+        self.kernels[kernel_id][0].iopub_socket.close()
+        self.kernels[kernel_id][0].stdin_socket.close()
+        self.send_signal(kernel_id, signal.SIGTERM)
+        self.kernels[kernel_id][1].join()
+        del self.kernels[kernel_id]
 
-    def fork_kernel(self, c):
-        km = KernelManager()
-        km.start_kernel()
-        c.send((km.shell_port, km.iopub_port, km.stdin_port, km.hb_port))
-        c.recv()
-        km.shutdown_kernel()
+    def interrupt_kernel(self, kernel_id):
+        """Interrupt a running kernel."""
+        self.send_signal(kernel_id, signal.SIGINT)
+
+    def restart_kernel(self, kernel_id):
+        ports = self.kernels[kernel_id][2]
+        self.kill_kernel(kernel_id)
+        self.start_kernel(kernel_id, Config({"IPKernelApp": ports}))
--- contrib/ipython-testing/untrusted_kernel_manager.py
+++ contrib/ipython-testing/untrusted_kernel_manager.py
@@ -8,12 +8,20 @@ class UntrustedMultiKernelManager:
         return self.fkm.start_kernel()
 
     def kill_kernel(self, kernel_id):
-        return self.fkm.kill_kernel(kernel_id)
+        self.fkm.kill_kernel(kernel_id)
+
+    def interrupt_kernel(self, kernel_id):
+        self.fkm.interrupt_kernel(kernel_id)
+
+    def restart_kernel(self, kernel_id, *args, **kwargs):
+        self.fkm.restart_kernel(kernel_id)
 
 if __name__ == "__main__":
     x = UntrustedMultiKernelManager()
     y = x.start_kernel()
     print y
     from time import sleep 
+    sleep(2)
+    x.restart_kernel(y["kernel_id"])
     sleep(2)
     x.kill_kernel(y["kernel_id"])
