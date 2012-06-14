e37e629aeb8619a2caaf76f57a759d9e0c852a6a
Ira Hanson <hanson.ira@gmail.com>
2012-06-14 02:03:21 -0700

    Fixing tab completion bugs

--- contrib/ipython-testing/static/interaction.js
+++ contrib/ipython-testing/static/interaction.js
@@ -1,4 +1,5 @@
 $(function() {
+    "use strict";
     function cursor_line(text, idx) {
         text = text.split("\n");
         while (text[0].length < idx) {
@@ -28,6 +29,38 @@ $(function() {
         return target.substr(0, i);
     }
 
+    function fill_completion(completion, textbox) {
+        var start = textbox.selectionStart;
+        var part = left_part_at_end(textbox.value.substring(0, start), completion);
+        textbox.value = textbox.value.substring(0, start) + completion.substr(part.length) + textbox.value.substring(start);
+        textbox.selectionStart = start + completion.length - part.length;
+        textbox.focus();
+    }
+
+    function show_completions(completions, dest, textbox) {
+        var span = document.createElement("span");
+        for (var i = 0; i < completions.length; i++) {
+            var item = document.createElement("span");
+            item.appendChild(document.createTextNode(completions[i]));
+            item.style.cursor = "pointer";
+            if (i > 0) {
+                item.style.marginLeft = "1em";
+            }
+            item.addEventListener("click", function (i) {
+                return function () {
+                    fill_completion(completions[i], textbox);
+                    dest.style.display = "none";
+                }
+            }(i));
+            span.appendChild(item);
+        }
+        if (dest.hasChildNodes()) {
+            dest.removeChild(dest.firstChild);
+        }
+        dest.appendChild(span);
+        dest.style.display = "inherit";
+    }
+
     var base_url = "kernel";
     var test_kernel = new IPython.Kernel(base_url);
     test_kernel.start();
@@ -52,73 +85,70 @@ $(function() {
     };
     var callbacks = {"output": output_callback};
 
-    (function() {
-        var listener = null;
+    (function f() {
         if (test_kernel.shell_channel === null || test_kernel.iopub_channel === null) {
-            var fn = arguments.callee;
             var _this = this;
-            setTimeout(function(){fn.call(_this);}, 200);
+            setTimeout(function(){f.call(_this);}, 200);
         } else {
             var codebox = $("#codebox");
             codebox.keydown(function (event) {
-                if (event.which === 9) {
+                if (event.keyCode === 9) {
                     event.preventDefault();
                 }
             });
+            var completions = [];
             codebox.keypress(function (event) {
                 if (event.keyCode === 9 && codebox[0].selectionStart !== undefined) {
                     event.preventDefault();
                     var lines = codebox.val().split("\n");
-                    var line = cursor_line(codebox.val(), codebox.selectionStart);
+                    var line = cursor_line(codebox.val(), codebox[0].selectionStart);
                     if (should_complete(codebox)) {
-                        if (listener !== null) {
-                            codebox.unbind("keypress", listener);
-                        }
                         test_kernel.complete(line[1], line[0], {"complete_reply": function (content) {
-                            if (content.matches.length > 1) {
-                                $("#completion").text(content.matches.join("   "));
-                                $("#completion").css("display", "inherit");
-                                if (listener !== null) {
-                                    codebox.unbind("keypress", listener);
-                                }
-                                listener = function (event) {
-                                    if (String.fromCharCode(event.charCode).match(/\W/)) {
-                                        return;
-                                    }
-                                    var matched = content.matches, new_matched = [];
-                                    for (var i = 0; i < matched.length; i++) {
-                                        if (left_part_at_end(codebox.val().substring(0, codebox[0].selectionStart) +
-                                                String.fromCharCode(event.charCode), matched[i])) {
-                                            new_matched.push(matched[i]);
-                                        }
-                                    }
-                                    $("#completion").text(new_matched.join("   "));
-                                }
-                                codebox.bind("keypress", listener)
-                            } else if (content.matches.length === 1) {
-                                var matched = content.matches[0];
-                                var val = codebox.val().substring(0, codebox[0].selectionEnd);
-                                var part = left_part_at_end(val, matched);
-                                if (part.length) {
-                                    codebox.val(val.substring(0, codebox[0].selectionEnd) +
-                                            matched.substring(part.length, matched.length) +
-                                            val.substring(codebox[0].selectionEnd, val.length));
-                                }
-                                $("#completion").css("display", "none");
-                                if (listener !== null) {
-                                    codebox.unbind("keypress", listener);
-                                }   
+                            completions = content.matches;
+                            if (completions.length > 1) {
+                                show_completions(completions, document.getElementById("completion"), codebox[0]);
+                            } else if (completions.length === 1) {
+                                fill_completion(completions[0], codebox[0]);
+                                document.getElementById("completion").style.display = "none";
                             } else {
-                                $("#completion").css("display", "none");
+                                document.getElementById("completion").style.display = "none";
                             }
                         }});
                     } else if (codebox[0].selectionStart === codebox[0].selectionEnd) {
                         codebox.val(codebox.val().substring(0, codebox[0].selectionBegin) + "\t" +
                                 codebox.val().substring(codebox[0].selectionEnd, codebox.val().length));
                     }
-                } else if (event.keyCode === 8 || String.fromCharCode(event.charCode).match(/\W/)) {
-                    $("#completion").css("display", "none");
+                } else if (event.keyCode === 8 || String.fromCharCode(event.charCode).match(/\w/) && 
+                    !(event.ctrlKey || event.altKey || event.metaKey || (event.shiftKey && event.keyCode === 8))) {
+                    var comp = document.getElementById("completion");
+                    if (comp.style.display !== "none") {
+                        var new_completions = [];
+                        var new_val = codebox.val().substring(0, codebox[0].selectionStart);
+                        if (event.keyCode !== 8) {
+                            new_val += String.fromCharCode(event.charCode);
+                        } else {
+                            new_val = new_val.substring(0, new_val.length - 1);
+                        }
+                        for (var i = 0; i < completions.length; i++) {
+                            var part = left_part_at_end(new_val, completions[i]);
+                            if (part.length > 0) {
+                                new_completions.push(completions[i]);
+                            }
+                        }
+                        if (new_completions.length > 0) {
+                            show_completions(new_completions, comp, codebox[0]);
+                        } else {
+                            comp.style.display = "none";
+                        }
+                    } else {
+                        comp.style.display = "none";
+                    }
+                } else {
+                    document.getElementById("completion").style.display = "none"
                 }
+            });
+            $("#codebox").on("click focus blur", function () {
+                document.getElementById("completion").style.display = "none"
             });
             $("#evalbutton").on("click", function(event) {
                 $("#message_output").empty();
