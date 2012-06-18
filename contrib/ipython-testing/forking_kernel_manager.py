a75b2d5d83e6f61162981a94b053f7b9c0184baa
Ira Hanson <hanson.ira@gmail.com>
2012-06-18 18:19:34 -0500

    Make the FKM work with IPython 0.13.beta1

--- contrib/ipython-testing/forking_kernel_manager.py
+++ contrib/ipython-testing/forking_kernel_manager.py
@@ -5,8 +5,9 @@ import signal
 import tempfile
 import json
 import random
+import sys
 import interact
-from IPython.zmq.kernelapp import KernelApp
+from IPython.zmq.ipkernel import IPKernelApp
 from IPython.config.loader import Config
 from multiprocessing import Process, Pipe
 
@@ -14,36 +15,36 @@ class ForkingKernelManager:
     def __init__(self):
         self.kernels = {}
 
-    def fork_kernel(self, sage_dict, config, connection_file, q):
-        ka = KernelApp.instance(config=config, kernel_class="IPython.zmq.ipkernel.Kernel",
-                                connection_file=connection_file)
+    def fork_kernel(self, sage_dict, config, q):
+        ka = IPKernelApp.instance(config=config)
         ka.initialize([])
-
-        """ These are commented out because they're causing the entire thing to hang
         ka.kernel.shell.user_ns.update(sage_dict)
         ka.kernel.shell.user_ns.update(interact.classes)
-        ka.kernel.shell.user_ns["sys"]._interacts = interact.interacts
+        if "sys" in ka.kernel.shell.user_ns:
+            ka.kernel.shell.user_ns["sys"]._interacts = interact.interacts
+        else:
+            sys._interacts = interact.interacts
+            ka.kernel.shell.user_ns["sys"] = sys
         ka.kernel.shell.user_ns["interact"] = interact.interact_func(ka.session, ka.iopub_socket)
-        """
-        q.send("")
+        q.send({"ip": ka.ip, "key": ka.session.key, "shell_port": ka.shell_port,
+                "stdin_port": ka.stdin_port, "hb_port": ka.hb_port, "iopub_port": ka.iopub_port})
         q.close()
         ka.start()
 
-    def start_kernel(self, sage_dict={}, kernel_id=None, config=None):
+    def start_kernel(self, sage_dict=None, kernel_id=None, config=None):
         random.seed()
+        if sage_dict is None:
+            sage_dict = {}
         if kernel_id is None:
             kernel_id = str(uuid.uuid4())
         if config is None:
             config = Config()
-        connection_file = "%s/%s.json" % (tempfile.gettempdir(), kernel_id)
         p, q = Pipe()
-        proc = Process(target=self.fork_kernel, args=(sage_dict, config, connection_file, q))
+        proc = Process(target=self.fork_kernel, args=(sage_dict, config, q))
         proc.start()
-        p.recv()
+        connection = p.recv()
         p.close()
-        with open(connection_file) as f:
-            connection = json.loads(f.read())
-        self.kernels[kernel_id] = (proc, connection, connection_file)
+        self.kernels[kernel_id] = (proc, connection)
         return {"kernel_id": kernel_id, "connection": connection}
 
     def send_signal(self, kernel_id, signal):
@@ -63,10 +64,6 @@ class ForkingKernelManager:
         """Kill a running kernel."""
         try:
             self.send_signal(kernel_id, signal.SIGTERM)
-            try:
-                os.remove(self.kernels[kernel_id][2])
-            except:
-                pass
             del self.kernels[kernel_id]
             return True
         except:
