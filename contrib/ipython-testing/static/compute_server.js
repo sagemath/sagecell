0610524c764560ee7112b0eeadabdd13765d98a6
Ira Hanson <hanson.ira@gmail.com>
2012-07-03 17:00:52 -0500

    Disable silent mode again

--- contrib/ipython-testing/static/compute_server.js
+++ contrib/ipython-testing/static/compute_server.js
@@ -100,7 +100,7 @@ sagecell.Session = function (outputDiv, hide) {
 sagecell.Session.prototype.execute = function (code) {
     if (this.opened) {
         var callbacks = {"output": $.proxy(this.handle_output, this)};
-        this.set_last_request(null, this.kernel.execute(code, callbacks));
+        this.set_last_request(null, this.kernel.execute(code, callbacks, {"silent": false}));
     } else {
         this.deferred_code.push(code);
     }
