1ec60f1cc1a0e8b81d945898a44fc402128bd89d
Jason Grout <jason.grout@drake.edu>
2013-09-10 11:13:09 -0500

    Add helper script to launch sagecell on cloud.sagemath.com

---
+++ contrib/cloud/launch_sagecell.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import subprocess
+import uuid
+
+with open(os.path.join(os.environ['HOME'],".sagemathcloud/info.json")) as f: 
+    projectid=uuid.UUID(json.loads(f.read())['project_id'])
+
+import argparse                                                                                                                      
+parser = argparse.ArgumentParser(description='Launch a SageCell web server',                                                         
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)                                             
+
+# the default port is derived from the project id
+parser.add_argument('-p', '--port', type=int, default=50000+(projectid.int%10000), 
+                    help='port to launch the server') 
+
+args = parser.parse_args()                                                                                                           
+port = args.port
+baseurl = "/%s/port/%d/"%(projectid, port)
+command="../sage/sage web_server.py --port %d --interface tun0 --baseurl %s"%(port, baseurl)
+print "Go to:"
+print "https://cloud.sagemath.com%s"%baseurl
+print "Running:"
+print command
+subprocess.call(command, shell=True)
