6717586735b8011577fc3bdfa5a066ae5243e3c8
Jason Grout <jason.grout@drake.edu>
2013-10-04 20:31:51 -0500

    Add SageCellComm class that basically resets the timeout anytime a comm is created.

---
+++ comm.py
@@ -0,0 +1,9 @@
+from IPython.kernel.comm import Comm
+
+import sys
+
+class SageCellComm(Comm):
+    def __init__(self, *args, **kwargs):
+        sys._sage_.reset_kernel_timeout(float('inf'))
+        super(SageCellComm, self).__init__(*args, **kwargs)
+
