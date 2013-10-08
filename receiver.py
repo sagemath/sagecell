2ab28d02296390e2d00efc372c6befe3b1caf8a0
Jason Grout <jason.grout@drake.edu>
2013-10-08 07:51:51 -0500

    make the threejs function sys._sage_.threejs

--- receiver.py
+++ receiver.py
@@ -102,7 +102,7 @@ from sagenb.misc.support import automatic_names
         _sage_.javascript = misc.javascript
         _sage_.sent_files = {}
         import graphics
-        _sage_.show_3d_plot_using_threejs = graphics.show_3d_plot_using_threejs
+        _sage_.threejs = graphics.show_3d_plot_using_threejs
         def new_files(root='./'):
             import os
             import sys
