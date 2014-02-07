76cb8f94ce28b52002c336db95dea5a236e936b2
Jason Grout <jason.grout@drake.edu>
2014-02-07 05:16:19 -0600

    Changes to the launch_sagecell script for cloud

--- contrib/cloud/launch_sagecell.py
+++ contrib/cloud/launch_sagecell.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+# extra things you need to do
+# npm install -g inherits requirejs
+# ssh-keygen
+# cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
+
 import os
 import sys
 import json
 import subprocess
 import uuid
+
+SAGE='../sage '
 
 with open(os.path.join(os.environ['HOME'],".sagemathcloud/info.json")) as f:
     projectid=uuid.UUID(json.loads(f.read())['project_id'])
@@ -21,13 +28,23 @@ args = parser.parse_args()
 port = args.port
 baseurl = "/%s/port/%d/"%(projectid, port)
 tmp_dir = '/tmp/sagecell-%s'%uuid.uuid4()
-command="../sage/sage web_server.py --tmp_dir %s --port %d --interface tun0 --baseurl %s"%(tmp_dir, port, baseurl)
-print "Go to:"
+command=SAGE+"web_server.py --tmp_dir %s --port %d --interface tun0 --baseurl %s"%(tmp_dir, port, baseurl)
+print
 print "https://cloud.sagemath.com%s"%baseurl
 print
 print "Computations in %s"%tmp_dir
 print "Running: %s"%command
 
-subprocess.call("../sage/sage -sh -c 'make coffee'", shell=True)
-subprocess.call("../sage/sage -sh -c 'make -B'", shell=True)
+subprocess.call(SAGE+"-sh -c 'make coffee'", shell=True)
+subprocess.call(SAGE+"-sh -c 'make -B'", shell=True)
+stars="*"*60
+print
+print
+for i in range(3): print stars
+print
+print "https://cloud.sagemath.com%s"%baseurl
+print
+for i in range(3): print stars
+print "Computations in %s"%tmp_dir
+print "Running: %s"%command
 subprocess.call(command, shell=True)
