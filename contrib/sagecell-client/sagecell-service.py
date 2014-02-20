45b1e9415ff53cc1e6ae06f0d42d7bde544b0774
Jason Grout <jason.grout@drake.edu>
2014-02-20 08:08:33 -0600

    Add a retry option to the sagecell service checking
    It seems that every now and then, this service request returns a success message, but there is no 'stout' key in the reply dictionary, which means that the service is restarted every couple of hours.  We were able to reproduce this sort of failure on personal machines by throwing hundreds of similar service requests at a server, and had maybe one failure every 100 requests or so.  Diving deeper, it appeared that for some reason, the zmq message of the computation was not getting sent to the kernel, even though it claimed that it was sent.  Upgrading to zmq 4.x didn't seem to help.  While we investigate further, we'll put in this stopgap measure that just retries the request after a short delay.

--- contrib/sagecell-client/sagecell-service.py
+++ contrib/sagecell-client/sagecell-service.py
@@ -2,25 +2,41 @@
 
 # curl -k -sS -L --data-urlencode "accepted_tos=true" --data-urlencode "code=myvar,myothervar=3,4\nprint 1+2" --data-urlencode "user_variables=myvar" --data-urlencode "user_variables=myothervar" $url
 
-reply = 'reply does not exist'
-try:
-    import urllib
-    import urllib2
-    import json
-    import sys
-    from random import randint
-    a,b = randint(-2**31,2**31), randint(-2**31,2**31)
-    code = "print %s+%s"%(a,b)
-    # if you agree with the terms of service at /tos.html
-    data = urllib.urlencode(dict(code=code, accepted_tos="true"))
-    request = urllib2.urlopen(sys.argv[1]+'/service', data, timeout = 30)
-    reply = json.loads(request.read())
-    answer = int(reply['stdout'].strip())
+import urllib
+import urllib2
+import json
+import sys
+from random import randint
+import time
 
-    assert reply['success'] is True
-    assert a+b == answer
-except Exception as e:
-    import traceback
-    traceback.print_exc()
-    print "Reply: ",reply
-    exit(1)
+retries = 3
+
+for i in range(retries):
+    reply = {'success': False, 'msg': 'default message'}
+    try:
+        a,b = randint(-2**31,2**31), randint(-2**31,2**31)
+        code = "print %s+%s"%(a,b)
+        # if you agree with the terms of service at /tos.html
+        data = urllib.urlencode(dict(code=code, accepted_tos="true"))
+        request = urllib2.urlopen(sys.argv[1]+'/service', data, timeout = 30)
+        reply = json.loads(request.read())
+        assert reply['success'] is True
+
+        # this retry business is a temporary kludge until we figure out why, every few hours, we
+        # have a request that comes back as executed, but the stdout is not in the dictionary
+        # drilling down, it seems that the compute message never actually gets sent to the kernel
+        # and it appears the problem is in the zmq connection between the webserver and the kernel
+        if 'stdout' in reply or i==retries-1:
+            # check answer if we are in the last retry loop, no matter what
+            answer = int(reply['stdout'].strip())
+            assert a+b == answer
+            break
+        else:
+            time.sleep(0.5)
+            continue
+
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        print "Reply: ",reply
+        exit(1)
