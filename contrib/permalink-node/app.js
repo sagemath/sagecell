fda0db06464a89c743d0d15db94ee503477520cf
Jason Grout <jason.grout@drake.edu>
2014-04-04 17:17:31 -0500

    More work on nodejs server---storing in database
    Still have a problem: storing multiple times on a single ID will not overwrite first value, but also doesn't trigger a retry to store.

--- contrib/permalink-node/app.js
+++ contrib/permalink-node/app.js
@@ -4,14 +4,17 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var helenus = require('helenus');
 var config = require('./config');
+var zlib = require('zlib');
+var async = require('async');
+var crypto = require('crypto');
 
 var app = express();
 var pool = new helenus.ConnectionPool(config.cassandra_config);
+
 pool.connect(function(err){
     if(err){
 	throw(err);
@@ -29,13 +32,133 @@ pool.connect(function(err){
 	app.use(express.errorHandler());
     }
 
-    app.post('/', routes.set_permalink);
-    app.post('/permalink', routes.set_permalink);
-    app.get('/', routes.get_permalink);
-    app.get('/permalink', routes.get_permalink);
-
+    app.post('/', set_permalink);
+    app.get('/', get_permalink);
+    app.post('/permalink', set_permalink);
+    app.get('/permalink', get_permalink);
 
     http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
     });
 });
+
+function set_permalink(req, res){
+    var retval = {query: null, zip: null}
+    if (req.body.code === undefined) {
+	res.send(400);
+	return;
+    }
+    var code = req.body.code || "";
+    var language = req.body.language || "sage";
+    var interacts = req.body.interacts || "";
+    var query_ident = null;
+    actions = {
+	zip: async.apply(async.waterfall, 
+			 [async.apply(zlib.deflate, code),
+			 function(b, cb) {cb(null, b.toString('base64'))}]),
+	query: async.apply(store_permalink, code, language, interacts)
+    }
+    if (req.body.interacts) {
+	actions.interacts = async.apply(async.waterfall, 
+					[async.apply(zlib.deflate, code),
+					 function(b, cb) {cb(null, b.toString('base64'))}]);
+    }
+    async.parallel(actions,
+	function(err, results) {
+	    if (err) {return next(err);}
+	    if (req.body.n) {
+		results.n = parseInt(req.body.n);
+	    }
+	    if (req.body.frame === undefined) {
+		res.set("Access-Control-Allow-Origin", req.get("Origin") || "*");
+		res.set("Access-Control-Allow-Credentials", "true");
+	    } else {
+		results = '<script>parent.postMessage('+JSON.stringify(results)+',"*");</script>';
+	    }
+	    res.send(results);
+	}
+    );
+};
+
+function get_permalink (req, res) {
+    async.series([
+	function(cb) {
+	    var results = '"Looked up"'+req.query.q;
+	    // TODO actually look up results in database
+	    cb(null, results);
+	}], function(err, results) {
+	    res.set("Access-Control-Allow-Origin", req.get("Origin") || "*");
+	    res.set("Access-Control-Allow-Credentials", "true");
+	    res.jsonp(results);
+	})
+}
+
+function retrieve_permalink(ident, cb) {
+/*
+- select on ident; return error if ident is not found
+- update last_access record
+- update permalink_count record
+- return code, language, interacts fields
+*/
+
+}
+
+function store_permalink(code, language, interacts, cb) {
+    // calls cb(err, ident)
+    var count = 0;
+    debugger;
+    var ident = null;
+    async.whilst(
+        function () { return (ident === null) && (count < 3); },
+        function (callback) {
+            count++;
+            async.waterfall([
+                async.apply(random_string, count),
+                function(id, cb) {
+                    console.log(count, id);
+                    insert_permalink(id, code, language, interacts, function(err, results) {
+                        console.log(err, results[0].get('ident'));
+                        if (err || !results[0].get('[applied]')) {
+                            // continue
+                            callback();
+                        } else {
+                            // break
+                            ident = id;
+                            callback();
+                        }
+                    })
+                }
+            ]);
+        },
+        function (err) {
+            if (ident===null) {
+                cb(true, null)
+            } else {
+                cb(null, ident);
+            }
+        }
+    );
+}
+
+function insert_permalink(ident, code, language, interacts, cb) {
+    var query = 'INSERT INTO cellserver.permalinks (ident, code, language, interacts, created, last_access) VALUES (?, ?, ?, ?, dateof(now()), dateof(now())) IF NOT EXISTS;';
+    var params = [ident, code, language, interacts];
+    pool.cql(query, params, cb);
+}
+
+function random_string(ident_len, cb) {
+    var valid_chars = "012345679";//"abcdefghijklmnopqrstuwxyz";
+    var num_valid = valid_chars.length;
+    var value = new Array(ident_len);
+    async.waterfall([
+        // pseudoRandom because we don't want to error out---just give us non-crypto random bytes, please
+        async.apply(crypto.pseudoRandomBytes, ident_len),
+        function (randbytes, cb) {
+            // construct id
+            for (var i = 0; i<ident_len; i++) {
+                value[i] = valid_chars[randbytes[i] % num_valid]
+            }
+            cb(null, value.join(''));
+        }], cb);
+}
+
--- contrib/permalink-node/readme.md
+++ contrib/permalink-node/readme.md
@@ -1,2 +1,44 @@
 node install
 node app # port 3000
+
+Table structure
+
+- ident: short string, primary key
+- code: string (limited to perhaps 5k?)
+- language: string
+- interacts: string (will be json, but we dont have to inist on it
+- created: datetime
+- last_access: datetime (for purging old data when it gets too large
+- requested: integer -- how many times it has been requested
+
+
+
+Setting up Cassandra:
+
+    create keyspace cellserver with replication={'class':'SimpleStrategy','replication_factor':1};
+    USE cellserver;
+    CREATE TABLE permalinks (
+      ident varchar PRIMARY KEY,
+      code varchar,
+      language varchar,
+      interacts varchar,
+      created timestamp,
+      last_access timestamp,
+    );
+    CREATE TABLE permalink_count (
+      ident varchar PRIMARY KEY,
+      requested counter
+    );
+
+When creating an entry:
+
+- make up random short ident
+- insert into database (IF NOT EXISTS) -- if error, then retry
+- return ident
+
+When retrieving an entry
+
+- select on ident; return error if ident is not found
+- update last_access record
+- update permalink_count record
+- return code, language, interacts fields
