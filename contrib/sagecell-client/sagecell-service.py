c91fc45800ad401e607255ee693a8d9637efb396
Andrey Novoseltsev <novoselt@gmail.com>
2014-10-10 14:14:37 -0600

    Always make multiple checks if the server is responding before restarting it.

--- contrib/sagecell-client/sagecell-service.py
+++ contrib/sagecell-client/sagecell-service.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# curl -k -sS -L --data-urlencode "accepted_tos=true" --data-urlencode "code=myvar,myothervar=3,4\nprint 1+2" --data-urlencode "user_variables=myvar" --data-urlencode "user_variables=myothervar" $url
-
 import urllib
 import urllib2
 import json
@@ -16,7 +14,6 @@ def message(s):
     print "%s: %s"%(datetime.now(), s)
 
 for i in range(retries):
-    reply = {'success': False, 'msg': 'default message'}
     try:
         a,b = randint(-2**31,2**31), randint(-2**31,2**31)
         code = "print %s+%s"%(a,b)
@@ -24,24 +21,29 @@ for i in range(retries):
         data = urllib.urlencode(dict(code=code, accepted_tos="true"))
         request = urllib2.urlopen(sys.argv[1]+'/service', data, timeout = 30)
         reply = json.loads(request.read())
-        assert reply['success'] is True
 
         # this retry business is a temporary kludge until we figure out why, every few hours, we
         # have a request that comes back as executed, but the stdout is not in the dictionary
         # drilling down, it seems that the compute message never actually gets sent to the kernel
         # and it appears the problem is in the zmq connection between the webserver and the kernel
-        if 'stdout' in reply or i==retries-1:
-            # check answer if we are in the last retry loop, no matter what
+        #
+        # Also sometimes reply is unsuccessful, yet the server keeps running
+        # and other requests are serviced. Since a restart breaks all active
+        # interacts, better not to restart the server that "mostly works" and
+        # instead we'll just accumulate statistics on these random errors to
+        # help resolve them.
+        if reply['success'] and 'stdout' in reply:
             answer = int(reply['stdout'].strip())
             assert a+b == answer
-            break
+            exit(0)
         else:
-            message("stdout not in reply on try %d; retrying..."%i)
+            message("Reply %s. %d attempts left." % (reply, retries - 1 - i))
             time.sleep(0.5)
-            continue
 
     except Exception as e:
         import traceback
         traceback.print_exc()
-        message("Reply: %s"%reply)
         exit(1)
+
+message("%d unsuccessful attempts, the server is not working!" % retries)
+exit(1)
