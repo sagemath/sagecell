d0a4a9a10fc045963c8e344426ec31c2252bfffd
Andrey Novoseltsev <novoselt@gmail.com>
2015-07-17 14:57:45 -0600

    Use MathJax from its own CDN!

--- static/embedded_sagecell.js
+++ static/embedded_sagecell.js
@@ -213,7 +213,7 @@ sagecell.init = function (callback) {
             'text': 'MathJax.Hub.Config(' + JSON.stringify(mjConfig) + ');',
             'type': 'text/x-mathjax-config'
         });
-        load({"src": sagecell.URLs.root + "static/mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML"});
+        load({"src": "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"});
     }
     // Preload images
     new Image().src = sagecell.URLs.sage_logo;
