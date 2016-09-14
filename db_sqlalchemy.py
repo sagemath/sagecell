c381451ac5aa3d7d27a3999b2d55dc1b6678af36
Andrey Novoseltsev <novoselt@gmail.com>
2016-09-13 19:03:47 -0600

    Some code clean up

--- db_sqlalchemy.py
+++ db_sqlalchemy.py
@@ -3,23 +3,14 @@ SQLAlchemy Database Adapter
 ---------------------------
 """
 
-"""
-System library imports
-"""
 import string, random
 from datetime import datetime
 
-"""
-SQLAlchemy imports
-"""
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 
-"""
-Generic database adapter import
-"""
 import db
 
 class DB(db.DB):
@@ -41,24 +32,26 @@ class DB(db.DB):
         See :meth:`db.DB.new_exec_msg`
         """
         while True:
-            session_id = "".join(random.choice(string.lowercase) for _ in range(6))
-            message = ExecMessage(ident=session_id, code=code, language=language, interacts=interacts)
+            ident = "".join(random.choice(string.lowercase) for _ in range(6))
+            message = ExecMessage(
+                ident=ident, code=code, language=language, interacts=interacts)
             try:
                 self.dbsession.add(message)
                 self.dbsession.commit()
             except IntegrityError:
+                # ident was used before
                 self.dbsession.rollback()
             else:
                 break
-        callback(session_id)
+        callback(ident)
 
     def get_exec_msg(self, key, callback):
         """
         See :meth:`db.DB.get_exec_msg`
         """
-        msg = self.dbsession.query(ExecMessage).filter_by(ident = key).first()
+        msg = self.dbsession.query(ExecMessage).filter_by(ident=key).first()
         if msg:
-            msg.requested = ExecMessage.requested+1
+            msg.requested = ExecMessage.requested + 1
             self.dbsession.commit()
         if msg is None:
             raise LookupError
@@ -71,7 +64,7 @@ class ExecMessage(Base):
     Table of input messages in JSON form.
     """
     __tablename__ = "permalinks"
-    ident = Column(String, primary_key = True, index = True)
+    ident = Column(String, primary_key=True, index=True)
     code = Column(String)
     language = Column(String)
     interacts = Column(String)
