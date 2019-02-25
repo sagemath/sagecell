d64f4590bddd1f2c7ce66b176cdcb0d83b90e096
Frédéric Chapoton <fchapoton2@gmail.com>
2019-02-25 20:37:42 +0100

    cleanup launch_sagecell.py
    * remove import of sys
    * use python3 syntax for print
    * some pep8 formatting

--- contrib/cloud/launch_sagecell.py
+++ contrib/cloud/launch_sagecell.py
@@ -6,45 +6,46 @@
 # cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
 
 import os
-import sys
 import json
 import subprocess
 import uuid
-
-SAGE='../sage '
-
-with open(os.path.join(os.environ['HOME'],".sagemathcloud/info.json")) as f:
-    projectid=uuid.UUID(json.loads(f.read())['project_id'])
-
 import argparse
+
+SAGE = '../sage '
+
+with open(os.path.join(os.environ['HOME'], ".sagemathcloud/info.json")) as f:
+    projectid = uuid.UUID(json.loads(f.read())['project_id'])
+
 parser = argparse.ArgumentParser(description='Launch a SageCell web server',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 # the default port is derived from the project id
-parser.add_argument('-p', '--port', type=int, default=50000+(projectid.int%10000),
+parser.add_argument('-p', '--port', type=int, default=50000 + (projectid.int % 10000),
                     help='port to launch the server')
 
 args = parser.parse_args()
 port = args.port
-baseurl = "/%s/port/%d/"%(projectid, port)
-tmp_dir = '/tmp/sagecell-%s'%uuid.uuid4()
-command=SAGE+"web_server.py --tmp_dir %s --port %d --interface tun0 --baseurl %s"%(tmp_dir, port, baseurl)
-print
-print "https://cloud.sagemath.com%s"%baseurl
-print
-print "Computations in %s"%tmp_dir
-print "Running: %s"%command
+baseurl = "/%s/port/%d/" % (projectid, port)
+tmp_dir = '/tmp/sagecell-%s' % uuid.uuid4()
+command = SAGE + "web_server.py --tmp_dir %s --port %d --interface tun0 --baseurl %s" % (tmp_dir, port, baseurl)
+print()
+print("https://cloud.sagemath.com%s" % baseurl)
+print()
+print("Computations in %s" % tmp_dir)
+print("Running: %s" % command)
 
-subprocess.call(SAGE+"-sh -c 'make coffee'", shell=True)
-subprocess.call(SAGE+"-sh -c 'make -B'", shell=True)
-stars="*"*60
-print
-print
-for i in range(3): print stars
-print
-print "https://cloud.sagemath.com%s"%baseurl
-print
-for i in range(3): print stars
-print "Computations in %s"%tmp_dir
-print "Running: %s"%command
+subprocess.call(SAGE + "-sh -c 'make coffee'", shell=True)
+subprocess.call(SAGE + "-sh -c 'make -B'", shell=True)
+stars = "*" * 60
+print()
+print()
+for i in range(3):
+    print(stars)
+print()
+print("https://cloud.sagemath.com%s" % baseurl)
+print()
+for i in range(3):
+    print(stars)
+print("Computations in %s" % tmp_dir)
+print("Running: %s" % command)
 subprocess.call(command, shell=True)
