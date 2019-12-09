b908a3c6558f14737b484dd0b7307d42f2c15e47
Steve Kieffer <steve.kieffer@runbox.com>
2019-12-09 10:57:15 -0500

    Provide easy access to CodeMirror editor instance.

--- js/cell.js
+++ js/cell.js
@@ -235,6 +235,7 @@ function init(cellInfo, k) {
     var temp = editor.render(cellInfo.editor, input, cellInfo.collapse);
     var editorType = temp[0];
     var editorData = temp[1];
+    cellInfo.editorData = editorData;
     editorData.k = k;
     input.find(".sagecell_advancedTitle").on("click", function () {
         input.find(".sagecell_advancedFields").slideToggle();
